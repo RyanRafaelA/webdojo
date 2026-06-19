c8e108dc68c52010a761211132e51bb38d8bc486
RyanRafaelA <ryrafael461@gmail.com>
2026-06-19 10:30:14 -0300

    Trabalhando com Asserções e Esperas

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -1,5 +1,5 @@
 describe("Formulario de Consultoria", () => {
-  it("Deve solicitar consultoria individual", () => {
+  it.only("Deve solicitar consultoria individual", () => {
     cy.start();
     cy.submitLoginForm("papito@webdojo.com", "katana123");
 
@@ -80,8 +80,42 @@ describe("Formulario de Consultoria", () => {
 
     cy.contains("button", "Enviar formulário").click();
 
-    cy.contains(
-      "Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.",
-    ).should("be.visible");
+    cy.get(".modal", { timeout: 7000 })
+      .should("be.visible")
+      .find(".modal-content")
+      .should("be.visible")
+      .end(
+        "have.text",
+        "Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.",
+      );
+  });
+
+  it("Deve verificar os campos obrigatório", () => {
+    cy.start();
+    cy.submitLoginForm("papito@webdojo.com", "katana123");
+
+    cy.goTo("Formulários", "Consultoria");
+
+    cy.contains("button", "Enviar formulário").click();
+
+    const inputsWithErr = ["Nome Completo", "Email"];
+
+    inputsWithErr.forEach((input) => {
+      cy.contains("label", input)
+        .parent()
+        .find("p")
+        .should("be.visible")
+        .and("have.text", "Campo obrigatório")
+        .and("have.class", "text-red-400")
+        .and("have.css", "color", "rgb(248, 113, 113)");
+    });
+
+    cy.contains("label", "termos de uso")
+      .parent()
+      .find("p")
+      .should("be.visible")
+      .and("have.text", "Você precisa aceitar os termos de uso")
+      .and("have.class", "text-red-400")
+      .and("have.css", "color", "rgb(248, 113, 113)");
   });
 });
