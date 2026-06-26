f55007cd648a17cd6bc6bef3785e41d5dad6b178
RyanRafaelA <ryrafael461@gmail.com>
2026-06-26 14:56:04 -0300

    Gestão de massa de testes

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -1,3 +1,5 @@
+import { personal, company } from "../fixtures/consultancy.json";
+
 describe("Formulario de Consultoria", () => {
   beforeEach(() => {
     cy.login();
@@ -6,67 +8,54 @@ describe("Formulario de Consultoria", () => {
   });
 
   it("Deve solicitar consultoria individual", () => {
-    cy.get("input[placeholder='Digite seu nome completo']").type(
-      "Fernando Papito",
-    );
+    cy.get("input[placeholder='Digite seu nome completo']").type(personal.name);
 
-    cy.get("input[placeholder='Digite seu email']").type("papito@teste.com.br");
+    cy.get("input[placeholder='Digite seu email']").type(personal.email);
 
-    cy.get("input[placeholder='(00) 00000-0000']")
-      .type("11 99999-1000")
-      .should("have.value", "(11) 99999-1000");
+    cy.get("input[placeholder='(00) 00000-0000']").type(personal.phone);
+    //.should("have.value", "(11) 99999-1000");
 
     cy.contains("label", "Tipo de Consultoria")
       .parent()
       .find("select")
-      .select("Individual");
+      .select(personal.consultancyType);
 
-    cy.contains("label", "Pessoa Física")
-      .find("input")
-      .click()
-      .should("be.checked");
+    if (personal.personType === "cpf") {
+      cy.contains("label", "Pessoa Física")
+        .find("input")
+        .click()
+        .should("be.checked");
 
-    cy.contains("label", "Pessoa Jurídica")
-      .find("input")
-      .should("be.not.checked");
+      cy.contains("label", "Pessoa Jurídica")
+        .find("input")
+        .should("be.not.checked");
+    } else if (personal.personType === "cnpj") {
+      cy.contains("label", "Pessoa Jurídica")
+        .find("input")
+        .click()
+        .should("be.checked");
 
-    cy.contains("label", "CPF")
-      .parent()
-      .find("input")
-      .type("20534430066")
-      .should("have.value", "205.344.300-66");
+      cy.contains("label", "Pessoa Física")
+        .find("input")
+        .should("be.not.checked");
+    }
 
-    const discoveryChannels = [
-      "Instagram",
-      "LinkedIn",
-      "Udemy",
-      "YouTube",
-      "Indicação de Amigo",
-    ];
+    cy.contains("label", "CPF").parent().find("input").type(personal.document);
+    //.should("have.value", "205.344.300-66");
 
-    discoveryChannels.forEach((channel) => {
+    personal.discoveryChannels.forEach((channel) => {
       cy.contains("label", channel).find("input").check().should("be.checked");
     });
 
-    cy.get("input[type='file']").selectFile("./cypress/fixtures/teste.pdf", {
+    cy.get("input[type='file']").selectFile(personal.file, {
       force: true,
     });
 
     cy.get(
       "textarea[placeholder='Descreva mais detalhes sobre sua necessidade']",
-    ).type(
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    );
+    ).type(personal.description);
 
-    const techs = [
-      "Cypress",
-      "Selenium",
-      "WebDriverIO",
-      "Playwright",
-      "Robot Framework",
-    ];
-
-    techs.forEach((tech) => {
+    personal.techs.forEach((tech) => {
       cy.get("input[placeholder='Digite uma tecnologia e pressione Enter']")
         .type(tech)
         .type("{enter}");
@@ -77,7 +66,84 @@ describe("Formulario de Consultoria", () => {
         .should("be.visible");
     });
 
-    cy.contains("label", "termos de uso").find("input").check();
+    if (personal.terms === true) {
+      cy.contains("label", "termos de uso").find("input").check();
+    }
+
+    cy.contains("button", "Enviar formulário").click();
+
+    cy.get(".modal", { timeout: 7000 })
+      .should("be.visible")
+      .find(".modal-content")
+      .should("be.visible")
+      .and(
+        "have.text",
+        "Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.",
+      );
+  });
+
+  it("Deve solicitar consulta In Company", () => {
+    cy.get("input[placeholder='Digite seu nome completo']").type(company.name);
+
+    cy.get("input[placeholder='Digite seu email']").type(company.email);
+
+    cy.get("input[placeholder='(00) 00000-0000']").type(company.phone);
+    //.should("have.value", "(11) 99999-1000");
+
+    cy.contains("label", "Tipo de Consultoria")
+      .parent()
+      .find("select")
+      .select(company.consultancyType);
+
+    if (company.personType === "cpf") {
+      cy.contains("label", "Pessoa Física")
+        .find("input")
+        .click()
+        .should("be.checked");
+
+      cy.contains("label", "Pessoa Jurídica")
+        .find("input")
+        .should("be.not.checked");
+    } else if (company.personType === "cnpj") {
+      cy.contains("label", "Pessoa Jurídica")
+        .find("input")
+        .click()
+        .should("be.checked");
+
+      cy.contains("label", "Pessoa Física")
+        .find("input")
+        .should("be.not.checked");
+    }
+
+    cy.contains("label", "CNPJ").parent().find("input").type(company.document);
+    //.should("have.value", "205.344.300-66");
+
+    company.discoveryChannels.forEach((channel) => {
+      cy.contains("label", channel).find("input").check().should("be.checked");
+    });
+
+    cy.get("input[type='file']").selectFile(company.file, {
+      force: true,
+    });
+
+    cy.get(
+      "textarea[placeholder='Descreva mais detalhes sobre sua necessidade']",
+    ).type(company.description);
+
+    company.techs.forEach((tech) => {
+      cy.get("input[placeholder='Digite uma tecnologia e pressione Enter']")
+        .type(tech)
+        .type("{enter}");
+
+      cy.contains("label", "Tecnologias")
+        .parent()
+        .contains("span", tech)
+        .should("be.visible");
+    });
+
+    if (company.terms === true) {
+      cy.contains("label", "termos de uso").find("input").check();
+    }
 
     cy.contains("button", "Enviar formulário").click();
 
